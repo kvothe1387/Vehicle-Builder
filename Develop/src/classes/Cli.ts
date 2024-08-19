c28087963826e797c9c5ae61ce97c4c694ee96a5
David McCullough <dmccullough488@gmail.com>
2024-08-18 23:54:03 -0500

    Updated tow action

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -385,22 +385,33 @@ class Cli {
               this.vehicles[i].reverse();
             }
           }
-        }
-        // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
+        } else if (answers.action === 'Tow') {
+          // ***TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin) {
+              if (this.vehicles[i] instanceof Truck) {
+                this.findVehicleToTow(this.vehicles[i] as Truck);
+                return;
+              } else {
+                console.log('This vehicle cannot tow.');
+              }
+            }
+          }
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
+
         else if (answers.action === 'Select or create another vehicle') {
-          // start the cli to return to the initial prompt if the user wants to select or create another vehicle
-          this.startCli();
-          return;
-        } else {
-          // exit the cli if the user selects exit
-          this.exit = true;
-        }
-        if (!this.exit) {
-          // if the user does not want to exit, perform actions on the selected vehicle
-          this.performActions();
-        }
-      });
+            // start the cli to return to the initial prompt if the user wants to select or create another vehicle
+            this.startCli();
+            return;
+          } else {
+            // exit the cli if the user selects exit
+            this.exit = true;
+          }
+          if (!this.exit) {
+            // if the user does not want to exit, perform actions on the selected vehicle
+            this.performActions();
+          }
+        });
   }
 
   // method to start the cli
